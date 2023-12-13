e8980b5140581e35865f4528562b2edc1a0eaa84
cameronvoell <cameron@xmtp.com>
2023-12-13 15:23:49 -0800

    Fix wasm error when building against @xmtp/xmtp-js ^11.3.0-beta.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
+import wasm from "vite-plugin-wasm";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
+  plugins: [react(), pluginRewriteAll(), wasm()],
 });
